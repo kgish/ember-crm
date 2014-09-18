eb46856114cfa21bd65410a678c5a93a8d449673
Kiffin Gish <kiffin.gish@planet.nl>
2014-09-18 22:45:29 +0200

    Completed chapter 15: Showing a Lead. However, lead displayed at the bottom and not to the right as it should according to the tutorial

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,10 +1,10 @@
-// For more information see: http://emberjs.com/guides/routing/
-
 App.Router.reopen({
   location: 'auto',
   rootURL: '/'
 })
 
 App.Router.map(function() {
-  this.resource('leads', { path: '/' })
+  this.resource('leads', { path: '/' }, function() {
+    this.resource('lead', { path: '/leads/:id' });
+    })
 });
---
+++ app/assets/javascripts/routes/lead.js
@@ -0,0 +1,7 @@
+App.LeadRoute = Ember.Route.extend({
+
+  model: function(params) {
+    return this.store.find('lead', params.id);
+  }
+
+});
