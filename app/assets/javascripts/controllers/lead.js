e161976c78a47036b1f75c5135585e3c4f1b712d
Kiffin Gish <kiffin.gish@planet.nl>
2014-09-20 22:39:59 +0200

    Completed chapter 16: Editing a Lead Part 1

---
+++ app/assets/javascripts/controllers/lead.js
@@ -0,0 +1,14 @@
+App.LeadController = Ember.ObjectController.extend({
+
+  showUnsavedMessage: function() {
+    return this.get('isDirty') && !this.get('isSaving')
+  }.property('isDirty','isSaving'),
+
+  actions: {
+
+    saveChanges: function() {
+      if (this.get('model.isDirty')) this.get('model').save();
+    }
+  }
+
+});
